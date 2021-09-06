c90349202c299e3597481e39d8d2a91f40d10a2f
Yemi Shin <shiny@carleton.edu>
2021-09-06 15:13:02 -0500

    clean fileIO.js

--- fileIO.js
+++ fileIO.js
@@ -24,14 +24,6 @@
     'use strict';
     if (loadConfigFlag) { av.fzr.clearFzr_activeConfig_nutData_Fn(); }
     else { av.fzr.clearMainFzrFn(); }  // clear freezer (globals.js)
-    
-    //Change loading a workspace will change the freezer, but not parents or configuration
-  /*  av.parents.clearParentsFn();  //globals.js
-    av.dnd.ancestorBox.selectAll().deleteSelectedNodes();
-    av.dnd.ancestorBox.sync();
-    av.dnd.activeOrgan.selectAll().deleteSelectedNodes();
-    av.dnd.activeOrgan.sync();
-  */
  
     av.fzr.loadedFromFile = 'before oReg defined.';
     var oReq = new XMLHttpRequest();
@@ -69,7 +61,6 @@
           av.fio.processFiles(loadConfigFlag, 'av.fio.readZipWS');
         }  //do not load configfile
       };  //end of process files loops
-      
       
       //sort TestDishes here
       // av.dnd.sortDnD('fzTdish');
